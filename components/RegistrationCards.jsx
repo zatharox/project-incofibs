fb946f22ba9be96488a59cdfb5d88c453343e011
zatharox <zatharox@gmail.com>
2022-10-19 01:05:30 +0530

    commit-now

--- components/RegistrationCards.jsx
+++ components/RegistrationCards.jsx
@@ -1,71 +1,71 @@
 
 const RegistrationCards = () => {
   return (
-    <div className="registrationCards">
-    <div className="grid">
-      <div className="grid-item">
-        <div className="card">
-          <div className="card-content">
-            <div className="Bird-container">  <h1 className="card-header" style={{textAlign:"center",marginTop: "15px"}} >Early Bird</h1></div>
+    <div className="registrationCards ashish" >
+    <div class="grid">
+      <div class="grid-item">
+        <div class="card">
+          <div class="card-content">
+            <div class="Bird-container">  <h1 class="card-header" >Early Bird</h1></div>
 
-            <div className="card-text">
-            <h2 style={{textAlign:"left",letterSpacing:".07rem",marginLeft: "15px"}}>  Till Oct 15,2022</h2>
+            <div class="card-text">
+            <h2 class="ashish_ka_h2">  Till Oct 15,2022</h2>
             </div>
-            <hr/>
-            <h3 style={{textAlign:"center"}}>Member Student <span style={{padding: 0,marginLeft: "25px"}}>₹300</span> </h3>
-          <hr/>
-          <h3 style={{textAlign:"center"}}>Member Faculty<span style={{padding: 0,marginLeft: "25px"}}>₹4000</span> </h3>
-        <hr/>
-        <h3 style={{textAlign:"center",paddingLeft:"16px"}}>Non Member Student <span style={{padding: 0,marginLeft: "25px"}}>₹3500</span> </h3>
-      <hr/>
-      <h3 style={{textAlign:"center",paddingLeft:"16px"}}>Non Member Faculty <span style={{padding: 0,marginLeft: "25px"}}>₹300</span> </h3>
-    <hr/>
-  <h3 style={{textAlign:"center",paddingLeft:"4px"}}>Foreign Delegates<span style={{padding: 0,marginLeft: "25px"}}>$100</span> </h3>
-            <button className="card-btn" style={{marginBottom: "20px"}}>Buy Now!</button>
+            <hr class="hr-ka" />
+            <h3 class="ashish_ka_h3_type1">Member Student <span class="paise" >₹300</span> </h3>
+          <hr class="hr-ka" />
+          <h3 class="ashish_ka_h3_type1" >Member Faculty<span class="paise">₹4000</span> </h3>
+        <hr class="hr-ka" />
+        <h3 class="ashish_ka_h3_type2" >Non Member Student <span class="paise">₹3500</span> </h3>
+      <hr class="hr-ka" />
+      <h3 class="ashish_ka_h3_type2" >Non Member Faculty <span class="paise">₹300</span> </h3>
+    <hr class="hr-ka" />
+  <h3 class="ashish_ka_h3_type3" >Foreign Delegates<span class="paise">$100</span> </h3>
+            <button class="card-btn" >Buy Now!</button>
           </div>
         </div>
       </div>
-      <div className="grid-item">
-        <div className="card">
-          <div className="card-content">
-            <div className="Bird-container">  <h1 className="card-header" style={{textAlign:"center",marginTop: "15px"}} >Regular</h1></div>
+      <div class="grid-item">
+        <div class="card">
+          <div class="card-content">
+            <div class="Bird-container">  <h1 class="card-header" >Regular</h1></div>
 
-            <div className="card-text">
-          <h2 style={{textAlign:"left",letterSpacing:".07rem",marginLeft: "15px"}}>Till Oct 31,2022</h2>
+            <div class="card-text">
+            <h2 class="ashish_ka_h2">  Till Oct 31,2022</h2>
             </div>
-            <hr/>
-            <h3 style={{textAlign:"center"}}>Member Student <span style={{padding: 0,marginLeft: "25px"}}>₹3500</span> </h3>
-          <hr/>
-          <h3 style={{textAlign:"center"}}>Member Faculty <span style={{padding: 0,marginLeft: "25px"}}>₹500</span> </h3>
-        <hr/>
-        <h3 style={{textAlign:"center",paddingLeft:"16px"}}>Non Member Student <span style={{padding: 0,marginLeft: "25px"}}>₹4000</span> </h3>
-      <hr/>
-      <h3 style={{textAlign:"center",paddingLeft:"16px"}}>Non Member Faculty <span style={{padding: 0,marginLeft: "25px"}}>₹6000</span> </h3>
-    <hr/>
-    <h3 style={{textAlign:"center",paddingLeft:"4px"}}>Foreign Delegates <span style={{padding: 0,marginLeft: "25px"}}>$150</span> </h3>
-            <button className="card-btn" style={{marginBottom: "20px"}}>Buy Now!</button>
+            <hr class="hr-ka" />
+            <h3 class="ashish_ka_h3_type1">Member Student <span class="paise" >₹3500</span> </h3>
+          <hr class="hr-ka" />
+          <h3 class="ashish_ka_h3_type1" >Member Faculty<span class="paise" >₹500</span> </h3>
+        <hr class="hr-ka" />
+        <h3 class="ashish_ka_h3_type2" >Non Member Student <span class="paise">₹4000</span> </h3>
+      <hr class="hr-ka" />
+      <h3 class="ashish_ka_h3_type2" >Non Member Faculty <span class="paise">₹6000</span> </h3>
+    <hr class="hr-ka" />
+  <h3 class="ashish_ka_h3_type3" >Foreign Delegates<span class="paise">$150</span> </h3>
+            <button class="card-btn" >Buy Now!</button>
           </div>
         </div>
       </div>
-      <div className="grid-item">
-        <div className="card">
-          <div className="card-content">
-            <div className="Bird-container">  <h1 className="card-header" style={{textAlign:"center",marginTop: "15px"}} >Late/on spot</h1></div>
+      <div class="grid-item">
+        <div class="card">
+          <div class="card-content">
+            <div class="Bird-container">  <h1 class="card-header" >Late/on spot</h1></div>
 
-            <div className="card-text">
-          <h2 style={{textAlign:"left",letterSpacing:".07rem",marginLeft: "15px"}}>Nov 01,2022 Onwards</h2>
+            <div class="card-text">
+            <h2 class="ashish_ka_h2">  Nov 01,2022 Onwards</h2>
             </div>
-            <hr/>
-            <h3 style={{textAlign:"center"}}>Member Student <span style={{padding: 0,marginLeft: "25px"}}>₹4000</span> </h3>
-          <hr/>
-          <h3 style={{textAlign:"center"}}>Member Faculty <span style={{padding: 0,marginLeft: "25px"}}>₹6000</span> </h3>
-        <hr/>
-        <h3 style={{textAlign:"center",paddingLeft:"16px"}}>Non Member Student <span style={{padding: 0,marginLeft: "25px"}}>₹500</span> </h3>
-      <hr/>
-      <h3 style={{textAlign:"center",paddingLeft:"16px"}}>Non Member Faculty <span style={{padding: 0,marginLeft: "25px"}}>₹7000</span> </h3>
-    <hr/>
-    <h3 style={{textAlign:"center",paddingLeft:"4px"}}>Foreign Delegates <span style={{padding: 0,marginLeft: "25px"}}>$200</span> </h3>
-            <button className="card-btn" style={{marginBottom: "20px"}}>Buy Now!</button>
+            <hr class="hr-ka" />
+            <h3 class="ashish_ka_h3_type1">Member Student <span class="paise" >₹4000</span> </h3>
+          <hr class="hr-ka" />
+          <h3 class="ashish_ka_h3_type1" >Member Faculty<span class="paise">₹6000</span> </h3>
+        <hr class="hr-ka" />
+        <h3 class="ashish_ka_h3_type2" >Non Member Student <span class="paise">₹500</span> </h3>
+      <hr class="hr-ka" />
+      <h3 class="ashish_ka_h3_type2" >Non Member Faculty <span class="paise">₹7000</span> </h3>
+    <hr class="hr-ka" />
+  <h3 class="ashish_ka_h3_type3" >Foreign Delegates<span class="paise">$200</span> </h3>
+            <button class="card-btn" >Buy Now!</button>
           </div>
         </div>
       </div>
